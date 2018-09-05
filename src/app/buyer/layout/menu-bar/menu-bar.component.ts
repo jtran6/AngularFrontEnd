8ab402a4b6abb91ca838c5f04e2c6856d58448c9
Josh Tran <josh.tran@procurementpartners.com>
2018-09-05 08:48:17 -0400

    got tab menu working based on click from user

--- src/app/buyer/layout/menu-bar/menu-bar.component.ts
+++ src/app/buyer/layout/menu-bar/menu-bar.component.ts
@@ -9,29 +9,34 @@ import { Router } from '../../../../../node_modules/@angular/router';
 })
 export class MenuBarComponent implements OnInit {
 
-  items: MenuItem[];
+  items: MenuItem[] = [];
+  tabClicked : string;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
     this.items = [
       {
-        label: 'Dashboard', icon: 'fa fa-dashboard',
+        label: 'Dashboard', 
+        icon: 'fa fa-dashboard',
         items:
           [
             {
               label: 'Home', command: (event) => {
                 // Make Home component appear
+                this.setTabMenu('dashboard');
               }
             },
             {
               label: 'Dashboard Reports', command: (event) => {
                 // Make Dashboard Reports component appear
+                this.setTabMenu('dashboard');
               }
             }
           ],
         command: (event) => {
           // Make Dashboard component appear
+          this.setTabMenu('dashboard');
         }
       },
       {
@@ -41,41 +46,49 @@ export class MenuBarComponent implements OnInit {
             {
               label: 'Shopping Carts', command: (event) => {
                 // Make Shopping Carts component appear
+                this.setTabMenu('order');
               }
             },
             {
               label: 'Recurring Orders', command: (event) => {
                 // Make Recurring Orders component appear
+                this.setTabMenu('order');
               }
             },
             {
               label: 'Order Entry', command: (event) => {
                 // Make Order Entry component appear
+                this.setTabMenu('order');
               }
             },
             {
               label: 'Quick Search', command: (event) => {
                 // Make Quick Search component appear
+                this.setTabMenu('order');
               }
             },
             {
               label: 'Advanced Search', command: (event) => {
                 // Make Advanced Search component appear
+                this.setTabMenu('order');
               }
             },
             {
               label: 'Browse', command: (event) => {
                 // Make Browse component appear
+                this.setTabMenu('order');
               }
             },
             {
               label: 'Vendor Direct', command: (event) => {
                 // Make Vendor Direct component appear
+                this.setTabMenu('order');
               }
             }
           ],
         command: (event) => {
           // Make Order component appear
+          this.setTabMenu('order');
         }
       },
       {
@@ -85,56 +98,67 @@ export class MenuBarComponent implements OnInit {
             {
               label: 'Order', command: (event) => {
                 // Make Order component appear
+                this.setTabMenu('transactions');
               }
             },
             {
               label: 'Order Approvals', command: (event) => {
                 // Make Order Approvals component appear
+                this.setTabMenu('transactions');
               }
             },
             {
               label: 'Invoices', command: (event) => {
                 // Make Invoices component appear
+                this.setTabMenu('transactions');
               }
             },
             {
               label: 'Invoice Approvals', command: (event) => {
                 // Make Invoice Approvals component appear
+                this.setTabMenu('transactions');
               }
             },
             {
               label: 'Hold', command: (event) => {
                 // Make Hold component appear
+                this.setTabMenu('transactions');
               }
             },
             {
               label: 'Offline Entries', command: (event) => {
                 // Make Offline Entries component appear
+                this.setTabMenu('transactions');
               }
             },
             {
               label: 'Order Resubmit', command: (event) => {
                 // Make Order Resubmit component appear
+                this.setTabMenu('transactions');
               }
             },
             {
               label: 'Offline Invoice Entry', command: (event) => {
                 // Make Offline Invoice Entry component appear
+                this.setTabMenu('transactions');
               }
             },
             {
               label: 'File Import', command: (event) => {
                 // Make File Import component appear
+                this.setTabMenu('transactions');
               }
             },
             {
               label: 'Requisitions', command: (event) => {
                 // Make Requisitions component appear
+                this.setTabMenu('transactions');
               }
             }
           ],
         command: (event) => {
           // Make Transactions component appear
+          this.setTabMenu('transactions');
         }
       },
       {
@@ -144,41 +168,49 @@ export class MenuBarComponent implements OnInit {
             {
               label: 'Items', command: (event) => {
                 // Make Items component appear
+                this.setTabMenu('reports');
               }
             },
             {
               label: 'Purchasing', command: (event) => {
                 // Make Purchasing component appear
+                this.setTabMenu('reports');
               }
             },
             {
               label: 'Budgets', command: (event) => {
                 // Make Budgets component appear
+                this.setTabMenu('reports');
               }
             },
             {
               label: 'Approvals', command: (event) => {
                 // Make Approvals component appear
+                this.setTabMenu('reports');
               }
             },
             {
               label: 'Transactions', command: (event) => {
                 // Make Transactions component appear
+                this.setTabMenu('reports');
               }
             },
             {
               label: 'GL Spend', command: (event) => {
                 // Make GL Spend component appear
+                this.setTabMenu('reports');
               }
             },
             {
               label: 'Admin', command: (event) => {
                 // Make Admin component appear
+                this.setTabMenu('reports');
               }
             }
           ],
         command: (event) => {
           // Make Reports component appear
+          this.setTabMenu('reports');
         }
       },
       {
@@ -188,26 +220,31 @@ export class MenuBarComponent implements OnInit {
             {
               label: 'Vendors', command: (event) => {
                 // Make Vendors component appear
+                this.setTabMenu('vendorManagement');
               }
             },
             {
               label: 'Items', command: (event) => {
                 // Make Items component appear
+                this.setTabMenu('vendorManagement');
               }
             },
             {
               label: 'Item Prices', command: (event) => {
                 // Make Item Prices component appear
+                this.setTabMenu('vendorManagement');
               }
             },
             {
               label: 'Order Guides', command: (event) => {
                 // Make Order Guides component appear
+                this.setTabMenu('vendorManagement');
               }
             }
           ],
         command: (event) => {
           // Make Vendor Management component appear
+          this.setTabMenu('vendorManagement');
         }
       },
       {
@@ -217,21 +254,25 @@ export class MenuBarComponent implements OnInit {
             {
               label: 'Users', command: (event) => {
                 // Make Users component appear
+                this.setTabMenu('security');
               }
             },
             {
               label: 'Jobs', command: (event) => {
                 // Make Jobs component appear
+                this.setTabMenu('security');
               }
             },
             {
               label: 'System', command: (event) => {
                 // Make System component appear
+                this.setTabMenu('security');
               }
             }
           ],
         command: (event) => {
           // Make Security component appear
+          this.setTabMenu('security');
         }
       },
       {
@@ -241,66 +282,79 @@ export class MenuBarComponent implements OnInit {
             {
               label: 'Locations', command: (event) => {
                 // Make Locations component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'Business Units', command: (event) => {
                 // Make Business Units component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'GL Segments', command: (event) => {
                 // Make GL Segments component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'GL Accounts', command: (event) => {
                 // Make GL Accounts component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'GL Codes', command: (event) => {
                 // Make GL Codes component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'GL Groups', command: (event) => {
                 // Make GL Groups component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'AP Import', command: (event) => {
                 // Make AP Import component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'News Message', command: (event) => {
                 // Make News Message component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'Order Rules', command: (event) => {
                 // Make Order Rules component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'Invoice Rules', command: (event) => {
                 // Make Invoice Rules component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'Approval Notifications', command: (event) => {
                 // Make Approval Notifications component appear
+                this.setTabMenu('administration');
               }
             },
             {
               label: 'Import Wizard', command: (event) => {
                 // Make Import Wizard component appear
+                this.setTabMenu('administration');
               }
             }
           ],
         command: (event) => {
           // Make Administration component appear
+          this.setTabMenu('administration');
         }
       },
       {
@@ -310,31 +364,37 @@ export class MenuBarComponent implements OnInit {
             {
               label: 'Budgets', command: (event) => {
                 // Make Budgets component appear
+                this.setTabMenu('budget');
               }
             },
             {
               label: 'Budget Groups', command: (event) => {
                 // Make Budget Groups component appear
+                this.setTabMenu('budget');
               }
             },
             {
               label: 'Budget Periods', command: (event) => {
                 // Make Budget Periods component appear
+                this.setTabMenu('budget');
               }
             },
             {
               label: 'Budget Period Values', command: (event) => {
                 // Make Budget Period Values component appear
+                this.setTabMenu('budget');
               }
             },
             {
               label: 'Budget Imports', command: (event) => {
                 // Make Budget Imports component appear
+                this.setTabMenu('budget');
               }
             }
           ],
         command: (event) => {
           // Make Budget component appear
+          this.setTabMenu('budget');
         }
       },
       {
@@ -344,26 +404,31 @@ export class MenuBarComponent implements OnInit {
             {
               label: 'Change Password', command: (event) => {
                 // Make Change Password component appear
+                this.setTabMenu('profile');
               }
             },
             {
               label: 'Emails', command: (event) => {
                 // Make Emails component appear
+                this.setTabMenu('profile');
               }
             },
             {
               label: 'Extended Users', command: (event) => {
                 // Make Extended Users component appear
+                this.setTabMenu('profile');
               }
             },
             {
               label: 'Alternate Shipping', command: (event) => {
                 // Make Alternate Shipping component appear
+                this.setTabMenu('profile');
               }
             }
           ],
         command: (event) => {
           // Make Profile component appear
+          this.setTabMenu('profile');
         }
       }
     ];
@@ -375,6 +440,11 @@ export class MenuBarComponent implements OnInit {
     // navigate to login page
     // inform user they have logged out
     this.router.navigate(['/login']);
+  }
+
+  setTabMenu(menuName: string){
+    this.tabClicked = menuName;
+    console.log(menuName);
   }
 
 }
--- src/app/buyer/layout/menu-bar/menu-tab/menu-tab.component.ts
+++ src/app/buyer/layout/menu-bar/menu-tab/menu-tab.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  Input
 } from '@angular/core';
 import {
   MenuItem
@@ -15,33 +16,230 @@ export class MenuTabComponent implements OnInit {
 
   items: MenuItem[];
   activeItem: MenuItem;
+  @Input() tabClicked: string = '';
   
   constructor() {}
 
-  ngOnInit() {
-    this.items = [{
-        label: 'Stats',
-        icon: 'fa fa-fw fa-bar-chart'
-      },
-      {
-        label: 'Calendar',
-        icon: 'fa fa-fw fa-calendar'
-      },
-      {
-        label: 'Documentation',
-        icon: 'fa fa-fw fa-book'
-      },
-      {
-        label: 'Support',
-        icon: 'fa fa-fw fa-support'
-      },
-      {
-        label: 'Social',
-        icon: 'fa fa-fw fa-twitter'
-      }
-    ];
-
-    this.activeItem = this.items[2];
+  ngOnChanges(){
+    this.setTabMenu(this.tabClicked);
   }
 
+  ngOnInit(){
+    this.setTabMenu(this.tabClicked);
+    //this.activeItem = this.items[0];
+  }
+
+  setTabMenu(tabClicked: string){
+    switch(tabClicked)
+    {
+      case '':
+      this.items = [
+        {
+          label: 'Announcements'
+        },
+        {
+          label: 'Dashboard Reports'
+        }
+      ];
+      break;
+      case 'dashboard':
+      this.items = [
+        {
+          label: 'Announcements'
+        },
+        {
+          label: 'Dashboard Reports'
+        }
+      ];
+      break;
+      case 'order':
+      this.items = [
+        {
+          label: 'Shopping Carts'
+        },
+        {
+          label: 'Recurring Orders'
+        },
+        {
+          label: 'Order Entry'
+        },
+        {
+          label: 'Quick Search'
+        },
+        {
+          label: 'Advanced Search'
+        },
+        {
+          label: 'Browse'
+        },
+        {
+          label: 'Vendor Direct'
+        }
+      ];
+      break;
+      case 'transactions': 
+      this.items = [
+        {
+          label: 'Orders'
+        },
+        {
+          label: 'Order Approvals'
+        },
+        {
+          label: 'Invoices'
+        },
+        {
+          label: 'Invoice Approvals'
+        },
+        {
+          label: 'Hold'
+        },
+        {
+          label: 'Offline Entries'
+        },
+        {
+          label: 'Order Resubmit'
+        },
+        {
+          label: 'Offline Invoice Entry'
+        },
+        {
+          label: 'File Import'
+        },
+        {
+          label: 'Requisitions'
+        }
+      ];
+      break;
+      case 'reports':
+      this.items = [
+        {
+          label: 'Items'
+        },
+        {
+          label: 'Purchasing'
+        },
+        {
+          label: 'Budgets'
+        },
+        {
+          label: 'Approvals'
+        },
+        {
+          label: 'Transactions'
+        },
+        {
+          label: 'GL Spend'
+        },
+        {
+          label: 'Admin'
+        }
+      ];
+      break;
+      case 'vendorManagement':
+      this.items = [
+        {
+          label: 'Vendors'
+        },
+        {
+          label: 'Items'
+        },
+        {
+          label: 'Item Prices'
+        },
+        {
+          label: 'Order Guides'
+        }
+      ];
+      break;
+      case 'security': 
+      this.items = [
+        {
+          label: 'Users'
+        },
+        {
+          label: 'Jobs'
+        },
+        {
+          label: 'System'
+        }
+      ];
+      break;
+      case 'administration': 
+      this.items = [
+        {
+          label: 'Locations'
+        },
+        {
+          label: 'Business Units'
+        },
+        {
+          label: 'GL Segments'
+        },
+        {
+          label: 'GL Accounts'
+        },
+        {
+          label: 'GL Codes'
+        },
+        {
+          label: 'GL Groups'
+        },
+        {
+          label: 'AP Import'
+        },
+        {
+          label: 'New Message'
+        },
+        {
+          label: 'Order Rules'
+        },
+        {
+          label: 'Invoice Rules'
+        },
+        {
+          label: 'Approval Notifications'
+        },
+        {
+          label: 'Import Wizard'
+        }
+      ];
+      break;
+      case 'budget':
+      this.items = [
+        {
+          label: 'Budgets'
+        },
+        {
+          label: 'Budget Groups'
+        },
+        {
+          label: 'Budget Periods'
+        },
+        {
+          label: 'Budget Period Values'
+        },
+        {
+          label: 'Budget Imports'
+        }
+      ];
+      break;
+      case 'profile':
+      this.items = [
+        {
+          label: 'Change Password'
+        },
+        {
+          label: 'Emails'
+        },
+        {
+          label: 'ExtUsers'
+        },
+        {
+          label: 'Alternate Shipping'
+        }
+      ];
+      break;
+    }
+  }
 }
